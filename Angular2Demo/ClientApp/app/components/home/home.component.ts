3ef87cc2d390670ef246a877614ba9fb0a20d5fb
jgradt <>
2017-11-04 15:25:24 -0500

    client logout functionality

--- Angular2Demo/ClientApp/app/components/home/home.component.ts
+++ Angular2Demo/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { AuthenticationService } from '../../services/authentication.service'
+
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+    constructor(private authService: AuthenticationService) { }
+
+    logout(): void {
+        this.authService.logout();
+        alert('logout successful');
+    }
 }
