acc3818460363bc251e6fa27a73812ef5a30847f
jgradt <>
2017-11-12 11:41:10 -0600

    refactor auto mapper customer profile

--- WebApiDemo/Data/Dto/CustomerDto.cs
+++ WebApiDemo/Data/Dto/CustomerDto.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
+using WebApiDemo.Data.Entities;
 
 namespace WebApiDemo.Models
 {
@@ -10,6 +12,7 @@ namespace WebApiDemo.Models
         public string Email { get; set; }
     }
 
+    // Fluent Validation
     public class CustomerDtoValidator : AbstractValidator<CustomerDto>
     {
         public CustomerDtoValidator()
@@ -19,5 +22,18 @@ namespace WebApiDemo.Models
             RuleFor(x => x.LastName).NotNull().Length(1, 50);
             RuleFor(x => x.Email).EmailAddress().MaximumLength(150);
         }
+    }
+
+    // AutoMapper
+    public class CustomerAutoMapProfile : Profile
+    {
+        public CustomerAutoMapProfile()
+        {
+            CreateMap<Customer, CustomerDto>()
+                .ForSourceMember(m => m.CreatedDate, opt => opt.Ignore())
+                .ForSourceMember(m => m.LastUpdatedDate, opt => opt.Ignore())
+                .ReverseMap();
+        }
+
     }
 }
